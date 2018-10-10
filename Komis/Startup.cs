27332a1c6e49f3e0f421e46b332a03fc8f3949bf
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-10-10 10:45:52 +0200

    3_5 Konfigurowanie Witryny

--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -15,20 +15,16 @@ namespace Komis
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
