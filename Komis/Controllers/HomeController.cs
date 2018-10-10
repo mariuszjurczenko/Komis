e86dd541d1604e7da581b3b5a7468b02d01e06b9
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-10-10 10:52:16 +0200

    4_6 Dodanie kontrolera

---
+++ Komis/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Komis.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {    
+            return View();
+        }
+    }
+}
