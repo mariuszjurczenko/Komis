48290bc327f85249c0952098d338013fc4cc081c
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-10-15 10:58:47 +0200

    5_4 dodanie EFCore

---
+++ Komis/Models/SamochodRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Komis.Models
+{
+    public class SamochodRepository : ISamochodRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public SamochodRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Samochod PobierzSamochodOId(int samochodId)
+        {
+            return _appDbContext.Samochody.FirstOrDefault(s => s.Id == samochodId);
+        }
+
+        public IEnumerable<Samochod> PobierzWszystkieSamochody()
+        {
+            return _appDbContext.Samochody;
+        }
+    }
+}
--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -1,17 +1,27 @@
 ﻿using Komis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Komis
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ISamochodRepository, MockSamochodRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ISamochodRepository, SamochodRepository>();
             services.AddMvc();
         }
 
